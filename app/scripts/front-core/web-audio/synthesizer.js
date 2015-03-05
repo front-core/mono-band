4c8f7ea715368f9b03920a05e4552c1698686d2c
Shindeok Kang <heavymery@gmail.com>
2015-03-06 01:55:00 +0900

    Web Audio の Synthesizer クラス実装中

--- app/scripts/front-core/web-audio/synthesizer.js
+++ app/scripts/front-core/web-audio/synthesizer.js
@@ -37,10 +37,11 @@ FrontCore.WebAudio.Synthesizer.prototype.constructor = FrontCore.WebAudio.Synthe
 
 /**
  * 指定した音名の周波数を返します。
- * @param {string} note - オクターブ番号付きの音名（A4, A#4, B4）
+ * @param {string} note - 音名（A4, A#4, B4 ...）
+ * @param {number} octave - オクターブ番号（0, 1, 2 ...）
  * @return {number}
  */
-FrontCore.WebAudio.Synthesizer.getHertz = function(note) {
+FrontCore.WebAudio.Synthesizer.getHertz = function(note, octave) {
   // TODO: A4 からの距離を計算
   var distance; 
 
@@ -48,6 +49,34 @@ FrontCore.WebAudio.Synthesizer.getHertz = function(note) {
   var hertz = 440 * Math.pow(1.0595, distance);
   
   return hertz;
+};
+
+/**
+ * 指定した音名のノート番号を返します。
+ * @param {string} note - 音名（A4, A#4, B4 ...）
+ * @param {number} octave - オクターブ番号（0, 1, 2 ...）
+ * @return {number}
+ */
+FrontCore.WebAudio.Synthesizer.getNoteNumber = function(note, octave) {
+  var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+  
+  // C -1 =  0  C 4 = 60
+  // C#-1 =  1  C#4 = 61
+  // D -1 =  2  D 4 = 62
+  // D#-1 =  3  D#4 = 63
+  // E -1 =  4  E 4 = 64
+  // F -1 =  5  F 4 = 65
+  // F#-1 =  6  F#4 = 66
+  // G -1 =  7  G 4 = 67
+  // G#-1 =  8  G#4 = 68
+  // A -1 =  9  A 4 = 69
+  // A#-1 = 10  A#4 = 70
+  // B -1 = 11  B 4 = 71
+  // C  0 = 12  C 5 = 72
+
+  var noteNumber = notes[note] + (12 * (octave + 1));
+
+  return noteNumber;
 };
 
 /**
